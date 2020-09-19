db42064bd3673650b98011e600df2598e7f0864b
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-09-18 23:38:52 -0700

    Project setup.

--- chateau/__init__.py
+++ chateau/__init__.py
@@ -11,3 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import flask
+
+
+def create_app() -> flask.app.Flask:
+    app = flask.Flask(__name__)
+
+    @app.route("/")
+    def hello_world() -> str:
+        return "Hello, World!"
+
+    return app
--- setup.py
+++ setup.py
@@ -33,4 +33,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    install_requires=["email_validator", "flask", "psycopg2", "Flask-WTF",],
 )
