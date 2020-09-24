bd4529c6988b49e599a86e2c7d28b19830086868
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-09-24 16:31:36 -0700

    Added minimal template and static files.

--- chateau/__init__.py
+++ chateau/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Softwerks LLC
+# Copyright 2020 Softwerks LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,6 +20,6 @@ def create_app() -> flask.app.Flask:
 
     @app.route("/")
     def hello_world() -> str:
-        return "Hello, World!"
+        return flask.render_template("base.html")
 
     return app
