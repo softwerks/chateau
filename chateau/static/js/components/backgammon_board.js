7418224b8e2ec8e2b454f55d6d5fcc22af800fe7
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2021-02-12 19:29:19 -0800

    Refactored. Added data attributes.

--- chateau/static/js/components/backgammon_board.js
+++ chateau/static/js/components/backgammon_board.js
@@ -12,109 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const range = (start, stop, step) =>
-    Array.from(
-        { length: (stop - start) / step + 1 },
-        (_, i) => start + i * step
-    );
-
-function checkers(match, position) {
-    if (match.player == 1)
-        position = position.reverse().map((n) => (n != 0 ? -n : n));
-
-    const split = {
-        top: position.slice(0, position.length / 2).reverse(),
-        bottom: position.slice(position.length / 2),
-    };
-
-    let checkers = Array.from({ length: 10 }, (_, i) =>
-        Array.from({ length: split['top'].length })
-    );
-
-    for (const half in split) {
-        for (let col = 0; col < split[half].length; col++) {
-            let row = half == 'top' ? 0 : checkers.length - 1;
-            for (let i = 0; i < Math.abs(split[half][col]); i++) {
-                if (Math.abs(split[half][col]) > 5 && i == 4) {
-                    checkers[row][col] = Math.abs(split[half][col]);
-                    break;
-                }
-                checkers[row][col] = split[half][col] > 0 ? '⚪' : '⚫';
-                row += half == 'top' ? 1 : -1;
-            }
-        }
-    }
-
-    return checkers;
-}
-
 class BackgammonBoard extends HTMLElement {
+    static checker_0 = '⚪';
+    static checker_1 = '⚫';
+    static checker_rows = 10;
+    static max_checkers = 5;
     static dice = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
-
-    static render(match, position) {
-        const points = checkers(match, position.board_points);
-        const bar = checkers(match, [
-            position.player_bar,
-            position.opponent_bar,
-        ]);
-        const off = checkers(match, [
-            position.player_off,
-            position.opponent_off,
-        ]);
-
-        let board = [];
-        board.push(match.player == 0 ? range(12, 1, -1) : range(13, 24, 1));
-        board.push(...points);
-        board.push(match.player == 0 ? range(13, 24, 1) : range(12, 1, -1));
-        bar.unshift(undefined);
-        bar.push(undefined);
-        for (let row = 0; row < bar.length; row++) {
-            board[row].splice(6, 0, bar[row]);
-        }
-        off.unshift(undefined);
-        off.push(undefined);
-        for (let row = 0; row < off.length; row++) {
-            board[row].push(off[row]);
-        }
-        let dice = Array.from({ length: 14 });
-        dice[match.player == 0 ? 2 : 9] =
-            BackgammonBoard.dice[match.dice[0] - 1];
-        dice[match.player == 0 ? 3 : 10] =
-            BackgammonBoard.dice[match.dice[1] - 1];
-        board.splice(6, 0, dice);
-        let double = Array.from({ length: 13 });
-        let double_pos = 6;
-        if (match.cube_holder == 0) double_pos = 1;
-        else if (match.cube_holder == 1) double_pos = 11;
-        const double_val =
-            match.cube_holder == 3 ? 64 : Math.pow(2, match.cube_value);
-        double[double_pos] = double_val;
-        for (let row = 0; row < double.length; row++) {
-            board[row].unshift(double[row]);
-        }
-
-        const table = document.createElement('table');
-        table.part = 'board-table';
-        for (let row = 0; row < board.length; row++) {
-            let tr = table.insertRow();
-            for (let col = 0; col < board[row].length; col++) {
-                let td = tr.insertCell();
-                if (row == 0 || row == board.length - 1)
-                    td.part = 'board-td-num';
-                else if (col == 7) td.part = 'board-td-bar';
-                else td.part = 'board-td';
-                if (board[row][col] != undefined) {
-                    if (row == 6)
-                        if (col == 0) td.part = 'board-td-cube';
-                        else td.part = 'board-td-die';
-                    let tdText = document.createTextNode(board[row][col]);
-                    td.appendChild(tdText);
-                }
-            }
-        }
-
-        return table;
-    }
 
     constructor() {
         super();
@@ -124,14 +27,17 @@ class BackgammonBoard extends HTMLElement {
         div.part = 'board-div';
         shadowRoot.append(div);
 
+        let table = document.createElement('table');
+        div.appendChild(table);
+
         const websocketURL = this.getAttribute('websocketURL');
         const websocket = new WebSocket(websocketURL);
 
-        websocket.onmessage = function (event) {
+        websocket.onmessage = (event) => {
             const game = JSON.parse(event.data);
-            console.log(game);
-            div.innerHTML = '';
-            div.appendChild(BackgammonBoard.render(game.match, game.position));
+            this.match = game.match;
+            this.position = game.position;
+            table.replaceWith(this.render());
         };
 
         const form = document.createElement('form');
@@ -157,6 +63,201 @@ class BackgammonBoard extends HTMLElement {
             form.reset();
         });
         shadowRoot.append(form);
+    }
+
+    render() {
+        let board = [];
+
+        board.push(this.header());
+        board.push(
+            ...BackgammonBoard.enumerate_checkers(
+                this.match.player,
+                this.position.board_points
+            )
+        );
+        board.push(this.footer());
+
+        function annotate_points() {
+            for (let row = 1; row <= BackgammonBoard.checker_rows; row++) {
+                for (let col = 0; col < board[row].length; col++) {
+                    if (row <= BackgammonBoard.checker_rows / 2)
+                        board[row][col] = {
+                            value: board[row][col],
+                            data: { point: board[0][col].value },
+                        };
+                    else
+                        board[row][col] = {
+                            value: board[row][col],
+                            data: { point: board[board.length - 1][col].value },
+                        };
+                }
+            }
+        }
+
+        annotate_points();
+
+        let bar = BackgammonBoard.enumerate_checkers(this.match.player, [
+            this.position.player_bar,
+            this.position.opponent_bar,
+        ]);
+
+        let off = BackgammonBoard.enumerate_checkers(this.match.player, [
+            this.position.player_off,
+            this.position.opponent_off,
+        ]);
+
+        function annotate_bar_off(checkers, property) {
+            for (let row = 0; row < checkers.length; row++) {
+                if (row < checkers.length / 2)
+                    checkers[row] = {
+                        value: checkers[row],
+                        data: { [property]: 0 },
+                        part: 'board-td-' + property,
+                    };
+                else
+                    checkers[row] = {
+                        value: checkers[row],
+                        data: { [property]: 1 },
+                        part: 'board-td-' + property,
+                    };
+            }
+        }
+
+        annotate_bar_off(bar, 'bar');
+        annotate_bar_off(off, 'off');
+
+        function insert_bar_off(checkers, index) {
+            checkers.unshift({ part: 'board-td-num' });
+            checkers.push({ part: 'board-td-num' });
+
+            for (let row = 0; row < checkers.length; row++) {
+                board[row].splice(index, 0, checkers[row]);
+            }
+        }
+
+        insert_bar_off(bar, board[0].length / 2);
+        insert_bar_off(off, board[0].length);
+
+        let dice = Array.from({ length: board[0].length });
+        dice[this.match.player == 0 ? 2 : 9] = {
+            value: BackgammonBoard.dice[this.match.dice[0] - 1],
+            data: { die: this.match.dice[0] },
+            part: 'board-td-die',
+        };
+        dice[this.match.player == 0 ? 3 : 10] = {
+            value: BackgammonBoard.dice[this.match.dice[1] - 1],
+            data: { die: this.match.dice[1] },
+            part: 'board-td-die',
+        };
+        dice[6] = { part: 'board-td-bar' };
+        board.splice(board.length / 2, 0, dice);
+
+        let cube = Array.from({ length: board.length });
+        let cube_pos = Math.floor(board.length / 2);
+        if (this.match.cube_holder == 0) cube_pos = 1;
+        else if (this.match.cube_holder == 1) cube_pos = board.length - 1;
+        const cube_val =
+            this.match.cube_holder == 3
+                ? 64
+                : Math.pow(2, this.match.cube_value);
+        cube[cube_pos] = {
+            value: cube_val,
+            data: { cube: this.match.cube_holder },
+            part: 'board-td-cube',
+        };
+        cube[0] = cube[cube.length - 1] = { part: 'board-td-num' };
+        for (let row = 0; row < cube.length; row++) {
+            board[row].unshift(cube[row]);
+        }
+
+        const table = document.createElement('table');
+        table.part = 'board-table';
+        for (let row = 0; row < board.length; row++) {
+            let tr = table.insertRow();
+            for (let col = 0; col < board[row].length; col++) {
+                let td = tr.insertCell();
+
+                let value = board[row][col]?.value;
+                if (value != undefined)
+                    td.appendChild(document.createTextNode(value));
+
+                let data = board[row][col]?.data;
+                if (data != undefined) Object.assign(td.dataset, data);
+
+                let part = board[row][col]?.part;
+                if (part != undefined) td.part = part;
+                else td.part = 'board-td';
+            }
+        }
+
+        return table;
+    }
+
+    header() {
+        return this.match.player == 0
+            ? BackgammonBoard.range(12, 1, -1)
+            : BackgammonBoard.range(13, 24, 1);
+    }
+
+    footer() {
+        return this.match.player == 0
+            ? BackgammonBoard.range(13, 24, 1)
+            : BackgammonBoard.range(12, 1, -1);
+    }
+
+    static range(start, stop, step) {
+        return Array.from({ length: (stop - start) / step + 1 }, (_, i) => ({
+            value: start + i * step,
+            part: 'board-td-num',
+        }));
+    }
+
+    static enumerate_checkers(player, checkers) {
+        function normalize() {
+            if (player == 1)
+                checkers = checkers.reverse().map((n) => (n != 0 ? -n : n));
+        }
+
+        normalize();
+
+        function split() {
+            return {
+                top: checkers.slice(0, checkers.length / 2).reverse(),
+                bottom: checkers.slice(checkers.length / 2),
+            };
+        }
+
+        const halves = split();
+        const columns = halves['top'].length;
+
+        let enumerated = Array.from(
+            { length: BackgammonBoard.checker_rows },
+            (_, i) => Array.from({ length: columns })
+        );
+
+        for (const half in halves) {
+            for (let col = 0; col < columns; col++) {
+                let row = half == 'top' ? 0 : enumerated.length - 1;
+                let owner = halves[half][col] > 0 ? 0 : 1;
+                let num_checkers = Math.abs(halves[half][col]);
+                for (let i = 0; i < num_checkers; i++) {
+                    if (
+                        num_checkers > BackgammonBoard.max_checkers &&
+                        i == BackgammonBoard.max_checkers - 1
+                    ) {
+                        enumerated[row][col] = num_checkers;
+                        break;
+                    }
+                    enumerated[row][col] =
+                        owner == 0
+                            ? BackgammonBoard.checker_0
+                            : BackgammonBoard.checker_1;
+                    row += half == 'top' ? 1 : -1;
+                }
+            }
+        }
+
+        return enumerated;
     }
 }
 
